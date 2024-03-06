dd1cf9a15e532d37899b97c654bad6f1131eb729
Gwenael <gpaindaveine@gmail.com>
2024-03-06 18:51:12 +0100

    Corrected version of distance (according to the formulas in the pdf)

--- Projet/c-skeleton/src/distance.c
+++ Projet/c-skeleton/src/distance.c
@@ -1,23 +1,25 @@
 #include <stdint.h>
 #include "../headers/distance.h"
+#include <stdlib.h>
 
-int64_t squared_manhattan_distance(const point_t *p1, const point_t *p2) {
-    int64_t dx = p2->x - p1->x;
-    int64_t dy = p2->y - p1->y;  
-    int64_t ar = abs(dx-dy); 
-    return ar*ar;
+
+int64_t squared_manhattan_distance(const point_t *p1, const point_t *p2, int dimension) {
+    int64_t sum = 0;
+    for (int i = 0; i < dimension; i++) {    
+    	int64_t dx = p2[i].x - p1[i].x;
+    	int64_t dy = p2[i].y - p1[i].y;  
+        sum +=	llabs(dx - dy);
+    }
+    return sum * sum;
 }
 
 
-int64_t squared_euclidean_distance(const point_t *p1, const point_t *p2) {
-    int64_t dx = p2->x - p1->x;
-    int64_t dy = p2->y - p1->y;
-    int64_t d = dx - dy;
-    return d*d;
-}
-
-
-static int64_t abs(int64_t n) {
-	if (n<0) return -n;
-	else return n;
+int64_t squared_euclidean_distance(const point_t *p1, const point_t *p2, int dimension) {
+    	int64_t sum = 0;
+	for (int i = 0; i < dimension; i++) {
+		int64_t dx = p2[i].x - p1[i].x;
+		int64_t dy = p2[i].y - p1[i].y;
+		sum += dx * dx - 2 * dx * dy + dy * dy;
+	}
+    	return sum;
 }
